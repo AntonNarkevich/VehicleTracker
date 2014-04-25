988a98e235eb1df5abf3a42fd337697f42dccf72
AntonNarkevich <17140th@gmail.com>
2014-04-25 10:39:51 +0300

    Fixes centrizeVertically. Changes config.

--- public/javascripts/jquery.centrizeVertically.js
+++ public/javascripts/jquery.centrizeVertically.js
@@ -3,19 +3,16 @@
 	'use strict';
 
 	/**
-	 * Changes margin-top of given element
-	 * to make the element centrized relative to
-	 * its offset parent.
-	 * @param $element - * jQuery element
-	 * @param {Object} options
+	 * Changes margin-top of given element.
+	 * to make the element centrized relatively to window
+	 * @param {Object} options - Options.
 	 * @param {boolean} options.isNegativeMarginAllowed - 
 	 * If true negative margins can be used for centrizing
-	 * else the plugin don't set negative marginTop values
-	 * If marginTop was negative centrizeVertically won't
-	 * modify it.
-	 * @param {boolean} options.isLoggingEnabled
-	 * If true enables logging
-	 * @returns {*} - * jQuery element for chaining
+	 * else if negative margin is needed for centrizing
+	 * centrizeVertically will set marginTop to 0.
+	 * @param {boolean} options.isLoggingEnabled -
+	 * If true enables logging.
+	 * @returns {*} - * jQuery element for chaining.
 	 */
 	$.fn.centrizeVertically = function changeMarginsToCentrize(options) {
 		var defaults = {
@@ -42,30 +39,30 @@
 		
 		return this.each(function() {
 			var $element = $(this);
+			var $window = $(window);
 
 			var height = $element.height();
 			//Auto throws away 'px'
 			var marginTop = parseInt($element.css('margin-top'), 10);
 			var offsetTop = $element.offset().top;
-			var offsetParentHeight = $element.offsetParent().height();
+			var windowHeight = $window.height();
 
 			log('height value is: ' + height);
 			log('marginTop value is: ' + marginTop);
 			log('offsetTop value is: ' + offsetTop);
-			log('offsetParentHeight value is: ' + offsetParentHeight);
+			log('windowHeight value is: ' + windowHeight);
 
-			var neededOffset = (offsetParentHeight - height) / 2;
+			var neededOffset = (windowHeight - height) / 2;
 			var marginChange = Math.round(neededOffset - offsetTop);
 
 			log('neededOffset value is: ' + neededOffset);
 			log('marginChange value is: ' + marginChange);
 
-			//Auto adds 'px'
 			var newMarginTop = marginTop + marginChange;
 			
 			if (newMarginTop < 0 && !settings.isNegativeMarginAllowed) {
-				log('Negative marginTop value. Aborting.');
-				return;
+				log('Negative marginTop value. Setting 0.');
+				newMarginTop = 0;
 			}
 
 			$element.css('margin-top', newMarginTop);
