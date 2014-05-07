a715a1eed0fb22a7df11b675fd0fb28807894853
AntonNarkevich <17140th@gmail.com>
2014-05-07 08:42:18 +0300

    Adds db export.

--- src/export/export.js
+++ src/export/export.js
@@ -10,23 +10,46 @@ var rekuire = require('rekuire');
 var logger = rekuire('logger');
 var keys = rekuire('keys.config.json');
 
-fs.mkdir(path.normalize('./temp/export'), function() {
-	var exportCommand = jade.renderFile('./src/export/export.jade', keys).split('\n').join(' & ');
+//TODO: Analize. When should I execute "done".
+function getExportArray(done) {
+	fs.mkdir(path.normalize('./temp/export'), function () {
+		var exportCommand = jade.renderFile('./src/export/export.jade', keys).split('\n').join(' & ');
 
-	var cp = spawn(process.env.comspec, ['/c', exportCommand]);
+		var cp = spawn(process.env.comspec, ['/c', exportCommand]);
 
-	cp.stdout.on('data', function (data) {
-		console.log(data.toString());
+		cp.stdout.on('data', function (data) {
+			//TODO: Use logger instead of console.
+			console.log(data.toString());
+		});
+
+		cp.stderr.on('data', function (data) {
+			console.error(data.toString());
+			done(data, null);
+		});
+
+		cp.on('close', function (code) {
+			console.log('exit code: ' + code);
+
+			var archive = new Zip();
+
+			//TODO: Is it synchronious? How can I make it async?
+			archive.addLocalFolder(path.normalize("./temp/export"));
+			archive.addLocalFile(path.normalize("./src/export/import.cmd"));
+
+//		archive.writeZip("./temp/export2.zip");
+
+			archive.toBuffer(function (buffer) {
+				logger.info('Made a buffer from zip.');
+				done(null, buffer);
+			}, function (err) {
+				logger.error('Cant make a buffer from zip.');
+				done(err, null);
+			});
+		});
 	});
+}
 
-	cp.stderr.on('data', function (data) {
-		console.error(data.toString());
-	});
-
-	cp.on('close', function (code) {
-		console.log('exit code: ' + code);
-
-
-	});
-});
+module.exports = {
+	getExportArray: getExportArray
+};
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,14 +2,17 @@
 
 var Request = require('tedious').Request;
 var router = require('express').Router();
+var mime = require('mime');
 
 var rekuire = require('rekuire');
+var exportDb = rekuire('export');
 var logger = rekuire('logger');
 var repository = rekuire('repository');
+var role = rekuire('roleStrategy');
 
 router.get('/init', function (req, res) {
-	repository.isAdminRegistered(function(err, isAdminRegistered) {
-		if(isAdminRegistered) {
+	repository.isAdminRegistered(function (err, isAdminRegistered) {
+		if (isAdminRegistered) {
 			res.render('httpError', {
 					message: 'Init has already happened. Admin is registered.',
 					code: 403
@@ -19,8 +22,16 @@ router.get('/init', function (req, res) {
 			res.render('init', { registrationFormAction: '/register/admin' });
 		}
 	});
+});
 
-
+router.get('/export', role.is('admin'), function (req, res) {
+	exportDb.getExportArray(function (err, zipBuffer) {
+		//TODO: Wht is mime abbreviation?
+		res.setHeader('Content-disposition', 'attachment; filename=vehicleTrackerDatabase.zip');
+		res.setHeader("Content-Type", mime.lookup('.zip'));
+		res.write(zipBuffer);
+		res.end();
+	});
 });
 
 module.exports = router;
