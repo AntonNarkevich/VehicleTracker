e410b4bf8ce9747514c1c5c2c25b428cba320401
AntonNarkevich <17140th@gmail.com>
2014-06-12 17:12:12 +0300

    Fixes profile page.

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -4,13 +4,23 @@ var router = require('express').Router();
 
 var rekuire = require('rekuire');
 var role = rekuire('roleStrategy');
+var logger = rekuire('logger');
+
 //TODO: Write to WebStorm about aout-requring/rekuiring.
-var repository = rekuire('repository');
+var database = rekuire('database');
 
 router.get('/:ownerId', role.is('signedIn'), function (req, res) {
 	var ownerId = req.param('ownerId');
 
-	repository.getProfile(ownerId, function(profileInfo) {
+	database.uspUserSelect(ownerId, function (err, data) {
+		if (err) {
+			logger.error(err);
+
+			throw err;
+		}
+
+		var profileInfo = data[0];
+
 		res.render('profile', { profileInfo: profileInfo });
 	});
 });
